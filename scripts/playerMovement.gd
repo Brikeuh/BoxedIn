224345043360209df523a93c1234681530bc0c8e
Russell C <russellgcheng@gmail.com>
2024-07-15 22:25:56 -0700

    Restructured the movement using requested change.

--- scripts/playerMovement.gd
+++ scripts/playerMovement.gd
@@ -1,20 +1,45 @@
 extends CharacterBody2D
 
-@export var speed = 400
+const max_speed = 300
+const accel = 1000
+const friction = 600
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+var input = Vector2.ZERO
 
 func _physics_process(delta):
-	get_input()
+	player_movement(delta)
 	move_and_slide()
+
+func get_input():
+	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	input.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	return input.normalized()
+
+func player_movement(delta):
+	input = get_input()
+	if input == Vector2.ZERO:
+		#instant stop
+		velocity = Vector2.ZERO
+		#character will slow down gradually
+		#if velocity.length() > (friction * delta):
+			#velocity -= velocity.normalized() * friction * delta
+		#else:
+			#velocity = Vector2.ZERO
+	elif velocity.length() < max_speed:
+		# gradually speeds character up until max speed is reached
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(max_speed)
+	else:
+		# player can now change direction (with no sliding) after reaching max speed
+		# stops when the player stops
+		velocity = (input * accel*100 * delta)
+		velocity = velocity.limit_length(max_speed)
 
 ## Called when the node enters the scene tree for the first time.
 #func _ready():
 	#pass # Replace with function body.
 
-
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#pass
+	
